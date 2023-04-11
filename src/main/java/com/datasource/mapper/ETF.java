6562a9d8f3bfa96766baafb00d9d89c56af7443e
yangxiangyu <2654527433@qq.com>
2023-04-11 17:32:52 +0800

    增加gateway全局网关过滤

--- src/main/java/com/datasource/mapper/ETF.java
+++ src/main/java/com/datasource/mapper/ETF.java
@@ -1,17 +1,17 @@
 package com.datasource.mapper;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
+//import com.baomidou.mybatisplus.annotation.IdType;
+//import com.baomidou.mybatisplus.annotation.TableId;
+//import com.baomidou.mybatisplus.annotation.TableName;
 
-@TableName("fund_info")
+//TableName("fund_info")
 public class ETF {
 
     private String code;
 
     private String name;
 
-    @TableId(type = IdType.AUTO)
+    //@TableId(type = IdType.AUTO)
     private Long id;
 
     private Boolean is_oct;
--- test/java/com/datasource/mapper/MapperTest.java
+++ test/java/com/datasource/mapper/MapperTest.java
@@ -8,7 +8,17 @@ public class MapperTest {
 
     @Test
     public void ETF(){
-
+        String str = "symbol=513100&code=11111";
+        StringBuffer stringBuffer = new StringBuffer();
+        String[] split = str.split("&");
+        for(String sp : split){
+            if(!sp.contains("code")){
+                stringBuffer.append(sp+"&");
+            }
+        }
+        String s = stringBuffer.toString();
+        s = s.substring(0,s.length() - 1);
+        System.out.println(s);
     }
 
 }
